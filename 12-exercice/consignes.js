792f894b1eb8d184c64c16b6b625fede36edb18b
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:20:22 +0100

    Add exercise 12

---
+++ 12-exercice/consignes.js
@@ -0,0 +1,7 @@
+/**
+ * A l'aide de la méthode foreach, parcourir le tableau et afficher les langages en majuscule.
+ */
+let languages = ["JavaScript", "HTML", "CSS", "nodejs"];
+
+// Write your code here.
+languages.forEach((lang) => console.log(lang, "->", lang.toUpperCase()));
