ce74d0ca32babdc933986e0c276d87773984585a
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 15:16:06 +0100

    Add bonus 2

---
+++ 99-exercices/consignes.js
@@ -0,0 +1,15 @@
+/**
+ * A l'aide de la méthode foreach, parcourir le tableau et afficher les prénoms.
+ */
+let prenoms = ["Jean", "Sarah", "John", "Ella"];
+
+// Write your code here.
+prenoms.forEach((prenom) => console.log(prenom));
+
+/**
+ * A l'aide de la méthode foreach, parcourir le tableau et afficher les prénoms en majuscule.
+ */
+let prenoms2 = ["Jean", "Sarah", "John", "Ella"];
+
+// Write your code here.
+prenoms2.forEach((prenom) => console.log(prenom.toUpperCase()));
