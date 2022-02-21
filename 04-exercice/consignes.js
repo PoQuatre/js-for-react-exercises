e4d58b83b3ec40afc7fba40e7fe8f59590de5d09
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:02:54 +0100

    Add exercise 4

---
+++ 04-exercice/consignes.js
@@ -0,0 +1,7 @@
+/**
+ * Faire une fonction qui prend un paramètre 'prenom' et affiche Bonjour suivi du prénom
+ */
+
+// Write your code here
+const greet = (prenom) => console.log(`Bonjour ${prenom}`);
+greet("Michel");
