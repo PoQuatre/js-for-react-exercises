4ca8e2ac799e644407da500e74e34cb86a20cbb7
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:24:11 +0100

    Add exercise 16

---
+++ 16-exercice/consignes.js
@@ -0,0 +1,14 @@
+/*
+ * À l'aide de la méthode filter, récupérer les produits dont le prix est supérieure ou égal à 2 euros
+ */
+let produits = [
+  { name: "tomate", prix: 2.99 },
+  { name: "lait d'amande", prix: 1.89 },
+  { name: "pastèque", prix: 1.89 },
+  { name: "basilic", prix: 0.99 },
+  { name: "melon", prix: 2 },
+];
+
+// Write your code here
+const products = produits.filter((produit) => produit.prix >= 2);
+console.log(products);
