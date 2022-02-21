61309815a0d90fe5d827509db47fd4df4e309349
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:12:22 +0100

    Add exercise 9

---
+++ 09-exercice/consignes.js
@@ -0,0 +1,13 @@
+/**
+ *  Faire une fonction anonyme qui affiche la soustraction de 2 nombres
+ */
+
+// Write your code here
+const subtract = function (num1, num2) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  console.log(num1, "-", num2, "=", num1 - num2);
+};
+
+subtract(8, 3);
