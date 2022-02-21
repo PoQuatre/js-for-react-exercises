b5a318660e0db23f907b182f6c31679f757cb321
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:26:42 +0100

    Add exercise 18

---
+++ 18-exercice/consignes.js
@@ -0,0 +1,10 @@
+/*
+ * À l'aide de la méthode map, créer un nouveau tableau qui contiendra les notes sur 20
+ * exemples: [7, 9] devient [14, 18]
+ *
+ */
+let notesSur10 = [2, 7, 9, 10, 6.5, 8.5];
+
+// Write your code here
+const notesSur20 = notesSur10.map((note) => note * 2);
+console.log(notesSur20);
