1f61c95b01ad03f3a6dbbe1d6e55815b8a4af5d5
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:11:45 +0100

    Add exercise 8

---
+++ 08-exercice/consignes.js
@@ -0,0 +1,13 @@
+/**
+ *  Faire une fonction qui affiche la soustraction de 2 nombres
+ */
+
+// Write your code here
+function subtract(num1, num2) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  console.log(num1, "-", num2, "=", num1 - num2);
+}
+
+subtract(8, 3);
