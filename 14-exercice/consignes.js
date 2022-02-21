d5230afa61af40d7697ec8e58f9fc4433dee40fb
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:21:37 +0100

    Add exercise 14

---
+++ 14-exercice/consignes.js
@@ -0,0 +1,8 @@
+/*
+ * À l'aide de la méthode filter, récupérer les notes supérieures à 10
+ */
+let nombres = [12, 6, 3, 4, 9, 15, 20];
+
+// Write your code here
+const notes = nombres.filter((num) => num > 10);
+console.log(notes);
