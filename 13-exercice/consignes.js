5492047ed60621f50b22561f661f0e96a2a6f256
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-21 14:20:29 +0100

    Add exercise 13

---
+++ 13-exercice/consignes.js
@@ -0,0 +1,7 @@
+/*
+ * A l'aide de la méthode foreach, parcourir le tableau et afficher nombres x2.
+ */
+let nombres = [12, 6, 3, 4, 9, 15, 67];
+
+// Write your code here
+nombres.forEach((num) => console.log(num, "-->", num * 2));
